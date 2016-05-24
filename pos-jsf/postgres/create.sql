3a4e48d40157861b9a75a3103a2f0be984fa424e
Marcus Vinicius <vmvini@hotmail.com>
2016-05-24 19:44:21 -0300

    payara and postgres Dockerfiles

---
+++ pos-jsf/postgres/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE pessoa(
+  id BIGSERIAL,
+  nome text not null,
+  PRIMARY KEY (id)
+);
---
+++ pos-jsf/postgres/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO pessoa (nome) VALUES ('Chiquinha');
+INSERT INTO pessoa (nome) VALUES ('Chaves');
+INSERT INTO pessoa (nome) VALUES ('Madruga');
+INSERT INTO pessoa (nome) VALUES ('Kiko');
