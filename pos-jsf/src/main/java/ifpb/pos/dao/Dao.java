a5a65cef20e4f2f3e945bceeb3374b1359c4b8fc
Antônio <Antnio@Junior-Dell>
2016-05-24 15:11:36 -0300

    salvar e listar pessoas

---
+++ pos-jsf/src/main/java/ifpb/pos/dao/Dao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifpb.pos.dao;
+
+import ifpb.pos.Pessoa;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Antônio
+ */
+public class Dao {
+    @PersistenceContext(name = "pos-jsf-crud")
+    EntityManager em;
+    
+    public void salvar(Pessoa pessoa){
+    em.persist(pessoa);
+    }
+    
+    public List<Pessoa> listarTodas(){
+    return em.createQuery("from Pessoa",Pessoa.class).getResultList();
+    }
+}
